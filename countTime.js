09ff0c31bf784c7f8e2d55989cb407dcd22435c0
Lenninlasd <lenninlasd@gmail.com>
2018-11-15 16:26:26 -0500

    add countTime

---
+++ countTime.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const filePath = process.argv[2];
+ if (!filePath) {
+    throw new Error('You have to specify the file path as an argument');
+}
+const logger = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+
+let seconds = 0;
+for (let i = 0; i < logger.length; i=i+2) {
+    let focus = logger[i].unixtime;
+    let blur = logger[i+1].unixtime;
+    seconds = seconds + (blur - focus)/1000;
+}
+
+const lastLoggerEl = logger[logger.length-1];
+console.log('total minutes in test: ', (seconds/60).toFixed(2));
+console.log('total boxes: ', lastLoggerEl.countedFrames);
+console.log('Boxes per minute: ', (lastLoggerEl.countedFrames * 60 /seconds).toFixed(2));
+console.log(`Time to create a box: ${(seconds/lastLoggerEl.countedFrames).toFixed(2)} seconds`);
